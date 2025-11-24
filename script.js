d50c5d86b61edced1eeca1d4c01ccf18df6220a2
KiPMaster127 <misha.azaranka@gmail.com>
2025-11-23 20:31:22 -0500

    Added Part 1 of the js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Creating student properties
+const studentProfile = {
+    fullName: "Misha",
+    age: 24,
+    isEnrolled: true,
+    subjects: [
+        "WRIT 120",
+        "CPAN 113",
+        "CPAN 111",
+        "CPAN 116",
+        "CPAN 133",
+        "CPAN 134"
+    ],
+    getSummary() {
+        return `Student: ${this.fullName}, Age: ${this.age}, Enrolled: ${this.isEnrolled}, Subjects: ${this.subjects.join(", ")}`;
+    }
+};
